8f2309a13f308d7960010ea9400a5c71eeca6e7c
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-08-24 16:56:53 +0300

    Created LoginViewController.swift and WelcomeViewController.swift

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Олег Федоров on 24.08.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
