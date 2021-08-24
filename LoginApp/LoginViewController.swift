594de762a37bee12ab9ea9b6bde6b4cfb1afcbc7
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-08-24 20:49:21 +0300

    Added goToWelcome segue and implemented transition

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    
+    @IBAction func loginButtonPressed() {
+        if userNameTF.text == "Kenny" && passwordTF.text == "Qwerty" {
+            performSegue(withIdentifier: "goToWelcomeView", sender: nil)
+        } else {
+            showAlert(
+                with: "Invalid login or password",
+                and: "Please, enter correct login and password"
+            )
+        }
+    }
     
     @IBAction func forgotUserNameButtonPressed() {
         showAlert(with: "Oops!", and: "Your Name is Kenny \u{1f44c}")
@@ -24,8 +39,10 @@ class LoginViewController: UIViewController {
     
     private func showAlert(with title: String, and message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default)
-        
+        let okAction = UIAlertAction(title: "OK", style: .default) {
+            _ in self.passwordTF.text = ""
+        }
+            
         alert.addAction(okAction)
         present(alert, animated: true)
     }
